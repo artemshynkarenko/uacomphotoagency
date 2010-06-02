38ef4e51e679c4d07918e395942a2d129e4e8186
oshynkarenko <oshynkarenko@ac71d338-6738-407e-feca-25ed3643013f>
2010-06-02 21:11:10 +0000

    Added php functionality to view image instead of text

---
+++ cgi/text2img.php
@@ -0,0 +1,40 @@
+<?php
+
+$itemHeight = 100;
+$itemWidth = 150;
+
+
+$font = './MyriadPro-Cond.otf'; //path to font you want to use
+$fontsize = 16; //size of font
+
+/*image generation code*/
+//create Image of size 350px x 75px
+$bg = imagecreatetruecolor($itemWidth, $itemHeight) or die ('Cannot initialize GD image stream.');
+imagealphablending($bg, false);
+imagesavealpha($bg, true);
+$trans_colour = imagecolorallocatealpha($bg, 0, 0, 0, 127);
+imagefilledrectangle($bg, 0, 0, imagesx($bg), imagesy($bg), $trans_colour);
+
+
+$grey0 = imagecolorallocate($bg, 128, 128, 128);
+$grey1 = imagecolorallocate($bg, 178, 178, 178);
+
+
+//Text to be written
+$itemStr = isset($_GET['text']) ? $_GET['text'] : "Menu Item";
+$box = imagettfbbox( $fontsize, 0, $font, $itemStr );
+//var_dump($box);
+$x = $itemWidth - ($box[2] - $box[0]);
+//Writes text to the image using fonts using FreeType 2
+imagettftext($bg, $fontsize, 0, $x, 35, $grey1, $font, $itemStr);
+imagettftext($bg, $fontsize, 0, $x, 85, $grey0, $font, $itemStr);
+
+
+//Create image
+header("Content-type: image/png");
+imagepng($bg);
+
+//destroy image
+ImageDestroy($bg);
+
+?>
