5af342e6906cb0fc7403096faff5e21b42ef4959
artem.shynkarenko <artem.shynkarenko@ac71d338-6738-407e-feca-25ed3643013f>
2010-06-22 22:14:37 +0000

    Finished parsing Picasa album feed

--- my-picasa.js
+++ my-picasa.js
@@ -1,13 +1,93 @@
-//
+﻿//
 //
 //
 
 var userName = "photoagency.lviv";
 var thumbnailSize='800';
-var pageName = "";
+
 var lastVisit = new Date();
 var level2MenuItemId = "albumList";
-var divScriptsEval = null;
+var level1MenuItemId = "categoryTopMenu";
+var contentItemId = "thumbnails";
+var labels = {
+	"wedding": "Весілля",
+	"album" : "Aльбом",
+	"archive" : "Архів",
+	"reportage" : "Репортаж",
+	"project" : "Проекти",
+	"portrait" : "Портрети",
+	"fashion" : "Мода",
+	"backstage" : "Backstage",
+	"top" : "Вибране"
+};
+
+function getLocalizedValue( labelAlias){
+	var localizedValue = labels[labelAlias];
+	if (!localizedValue){
+		localizedValue = labelAlias;
+	}
+	return localizedValue;
+}
+
+var pageName = "";
+var pageState = null;
+
+function getPageStateFromUrl(){
+	//alert("Start getPageStateFromUrl()");
+	var _GET = new Array();
+	var uriStr  = window.location.href.replace(/&amp;/g, '&');
+	var paraArr, paraSplit;
+	if(uriStr.indexOf('?') > -1){
+		var uriArr  = uriStr.split('?');
+		var paraStr = uriArr[1];	
+		if(paraStr.indexOf('&') > -1){
+			paraArr = paraStr.split('&');
+		}
+		else{
+			paraArr = new Array(paraStr);
+		}
+		for(var i = 0; i < paraArr.length; i++){
+			paraArr[i] = paraArr[i].indexOf('=') > -1 ? paraArr[i] : paraArr[i] + '=';
+			paraSplit  = paraArr[i].split('=');
+			_GET[paraSplit[0]] = decodeURI(paraSplit[1].replace(/\+/g, ' '));
+		}
+	}
+	//alert("Url parsed");
+	var level1 = _GET["level1"];;
+	if(!level1){
+		level1 = "wedding";
+	}
+	else{
+		level1 = level1.toLowerCase();
+	}
+	var level2 = _GET["level2"];
+	if(level2){
+		level2 = level2.toLowerCase();
+	}
+	else{
+		level2 = "top";
+	}
+	
+	var albumId = _GET["albumId"];
+	pageState = {"level1": level1, "level2" : level2, "albumId" : albumId};
+	//alert("End getPageStateFromUrl()");
+}
+
+function pageStateToUrl(newPageState){
+	//alert("Start pageStateToUrl");
+	var url = pageName + '?';
+	if (newPageState.level1){
+		url += 'level1=' + newPageState.level1;
+		if (newPageState.level2 && newPageState.level2 != 'undefined'){
+			 url += '&level2=' + newPageState.level2;
+			 if (newPageState.albumId && newPageState.albumId != 'undefined'){
+				 url += '&albumId=' + newPageState.albumId;
+			 }
+		}
+	}
+	//alert("End pageStateToUrl");
+	return url;
+}
 function _print(element, html){
 	//alert("Printing");
 	if (element){
@@ -18,22 +98,13 @@ function _print(element, html){
 
 function __printScriptInclude(src){
 	var scriptString = '<script type="text/javascript" src="' + src + '"></script>';
-	_print(divScriptsEval, scriptString);
+	_print($(document), scriptString);
 }
 function __printScriptBlock(script){
 	var scriptString = '<script type="text/javascript">' + script + '</s' + 'cript>';
-	_print(divScriptsEval, scriptString);
+	_print($(document), scriptString);
 }
 
-function endGetAlbumList(picasaJson){
-	//alert ("Start endGetAlbumList");
-	if (picasaJson.version != "1.0"){
-		alert("Picass version has changed. Site may not work as properly. Sorry :(");
-	}
-	var albumFeed = picasaJson.feed.entry;
-	buildLevel2Menu("wedding", albumFeed);
-	//alert ("End endGetAlbumList");
-}
 
 function beginGetAlbumsList(){
 	//alert("Start beginGetAlbumsList");
@@ -45,9 +116,29 @@ function beginGetAlbumsList(){
 	//alert("End beginGetAlbumsList");
 }
 
-function buildLevel2Menu(level1Name, albumFeed){
-	//alert ("Start buildLevel2Menu");
-	var level1NameLowerCase = level1Name.toLowerCase();
+function endGetAlbumList(picasaJson){
+	//alert ("Start endGetAlbumList");
+	if (picasaJson.version != "1.0"){
+		alert("Picass version has changed. Site may not work as properly. Sorry :(");
+	}
+	var albumFeed = picasaJson.feed.entry;
+	getPageStateFromUrl();
+	buildLevel1Menu(albumFeed);
+	buildLevel2Menu(albumFeed);
+	if(!pageState.albumId){
+		buildLevelContent(albumFeed);
+	}
+	else{
+		buildAlbumContent();
+	}
+	//alert ("End endGetAlbumList");
+}
+
+
+
+
+function buildLevel1Menu(albumFeed){
+	//alert ("Start buildLevel1Menu");
 	var menuItems = new Array();
 	for(var i = 0; i < albumFeed.length; ++i){
 		var metaJsonString = albumFeed[i].media$group.media$description.$t;
@@ -56,7 +147,52 @@ function buildLevel2Menu(level1Name, albumFeed){
 			continue;
 		var metaJson = JSON.parse(metaJsonString);
 		
-		if(!metaJson.level1 || metaJson.level1.toLowerCase() != level1NameLowerCase)
+		if(!metaJson.level1)
+			continue;
+		//alert("Level1 satisfied");
+		var level1String = metaJson.level1.toLowerCase();
+		var level1Items = level1String.split(',');
+		for(var k = 0; k < level1Items.length; ++k){
+			var level1 = level1Items[k];
+			var j = 0;
+			for (; j < menuItems.length; ++j){
+				if (menuItems[j].level1 == level1){
+					break;
+				}
+			}		
+			if (j == menuItems.length){
+				menuItems.push( {"level1": level1} );
+			}
+		}
+	}
+	//alert("Found " + menuItems.length + "top menuItems");
+	var menuHolder = $("#" + level1MenuItemId);
+	for(var i = 0; i < menuItems.length; ++i){
+		var menuItem =  menuItems[i];
+		if (menuItem.level1 == pageState.level1){
+			var menuHtml = '<li><h2><a href="' + pageStateToUrl(menuItem) + '">'
+ 				+ getLocalizedValue(menuItem.level1) + ' SELECTED</a></h2></li>';
+			_print ( menuHolder, menuHtml);
+		}
+		else{
+			var menuHtml = '<li><h2><a href="' + pageStateToUrl(menuItem) + '">'
+ 				+ getLocalizedValue(menuItem.level1) + '</a></h2></li>';
+			_print ( menuHolder, menuHtml);
+		}
+	}
+	//alert ("End buildLevel1Menu");
+}
+function buildLevel2Menu(albumFeed){
+	//alert ("Start buildLevel2Menu");
+	var menuItems = new Array();
+	for(var i = 0; i < albumFeed.length; ++i){
+		var metaJsonString = albumFeed[i].media$group.media$description.$t;
+		//alert("Raw metadata: " + metaJsonString);
+		if(!metaJsonString || metaJsonString == "")
+			continue;
+		var metaJson = JSON.parse(metaJsonString);
+		
+		if(!metaJson.level1 || metaJson.level1.toLowerCase().indexOf(pageState.level1) == -1)
 			continue;
 		//alert("Level1 satisfied");
 		var level2String = metaJson.level2.toLowerCase();
@@ -69,21 +205,93 @@ function buildLevel2Menu(level1Name, albumFeed){
 					++menuItems[j].count;
 					break;
 				}
+			}		
+			if (j == menuItems.length){
+				menuItems.push( {"level1": pageState.level1, "level2": level2, "count" : 1} );
 			}
 		}
-		if (j == menuItems.length){
-			menuItems.push( {"level2": level2, "count" : 1} );
-		}
 	}
-	//alert("Found " + menuItems.length + "items");
-	var menuHtml = "";
+	//alert("Found " + menuItems.length + " albumlist items");
+	var menuHolder = $("#" + level2MenuItemId);
 	for(var i = 0; i < menuItems.length; ++i){
 		var menuItem =  menuItems[i];
-		menuHtml += '<li><h3><a href="' + pageName + '?level1=' + level1Name + '&level2=' + menuItem.level2 + '">'
- 			+ menuItem.level2 + '&nbsp;(' + menuItem.count + ')</a></h3></li>';
+		if (menuItem.level2 == pageState.level2){
+			var menuHtml = '<li><h3><a href="' + pageStateToUrl(menuItem) + '">'
+ 				+ getLocalizedValue(menuItem.level2) + '&nbsp;(' + menuItem.count + ') SELECTED</a></h3></li>';
+			_print ( menuHolder, menuHtml);
+		}
+		else{
+			var menuHtml = '<li><h3><a href="' + pageStateToUrl(menuItem) + '">'
+ 				+ getLocalizedValue(menuItem.level2) + '&nbsp;(' + menuItem.count + ')</a></h3></li>';
+			_print ( menuHolder, menuHtml);
+		}
 	}
-	var menuHolder = $("#" + level2MenuItemId);
-	_print ( menuHolder, menuHtml);
 	//alert ("End buildLevel2Menu");
 }
 
+function buildLevelContent(albumFeed){
+	//alert ("Start buildLevelContent");
+	var albumsItems = new Array();
+	for(var i = 0; i < albumFeed.length; ++i){
+		var feedItem = albumFeed[i]
+		var metaJsonString = feedItem.media$group.media$description.$t;
+		//alert("Raw metadata: " + metaJsonString);
+		if(!metaJsonString || metaJsonString == "")
+			continue;
+		var metaJson = JSON.parse(metaJsonString);
+		
+		if(!metaJson.level1 || metaJson.level1.toLowerCase().indexOf(pageState.level1) == -1 )
+			continue;
+		if (!metaJson.level2 || metaJson.level2.toLowerCase().indexOf(pageState.level2) == -1 )
+			continue;
+		//alert("Level1, Level2 satisfied");
+		feedItem.media$group.media$description.$t = metaJson;
+		albumsItems.push( {"level1": pageState.level1, "level2": pageState.level2,  "jSon" : feedItem} );
+	}
+	//alert("Found " + albumsItems.length + " albums");
+	var contentHolder = $("#" + contentItemId);
+	for(var i = 0; i < albumsItems.length; ++i){
+		var albumItem =  albumsItems[i];
+		var idString = albumItem.jSon.id.$t;
+		//alert(idString);
+		var id_begin = idString.indexOf('albumid/')+8;
+  		var id_end = idString.indexOf('?');
+  		var id_base = idString.slice(id_begin, id_end);
+		albumItem.albumId = id_base;
+		var albumTitle = albumItem.jSon.title.$t;
+		var imageUrl = albumItem.jSon.media$group.media$content[0].url + '?imgmax=160&crop=1';
+		var albumHtml = '<p><a href="' + pageStateToUrl(albumItem) + '">'
+			+ '<img src="' + imageUrl + '" alt="' + albumTitle + '" class="pwimages" />'
+			+ albumTitle + '</a></p>';
+		_print ( contentHolder, albumHtml);
+	}
+	//alert ("End buildLevelContent");
+}
+
+function buildAlbumContent(){
+	//alert ("Start buildAlbumContent");
+	beginGetAlbumPhotos();
+	//alert ("End buildAlbumContent");
+}
+
+function beginGetAlbumPhotos(){
+	//alert ("Start beginGetAlbumPhotos");
+	var picasaImageListUrl = 'http://picasaweb.google.com/data/feed/base/user/' + userName +'/albumid/' + pageState.albumId
+			+ '?category=photo&alt=json&callback=endGetAlbumPhotos';
+	__printScriptInclude( picasaImageListUrl);
+	//alert ("End beginGetAlbumPhotos");
+}
+
+function endGetAlbumPhotos(photosFeed){
+	//alert ("Start endGetAlbumPhotos");
+	var entry = photosFeed.feed.entry;
+	var contentHolder = $("#" + contentItemId);
+	for(var i=0; i < entry.length; i++){
+		var imageItem = entry[i];
+		var imgUrl = imageItem.media$group.media$content[0].url;
+		var imageHtml = '<p><a class="gallery" href="' + imgUrl + '">'
+			+ '<img src="' + imgUrl + '?imgmax=160&crop=1" class="pwimages" /></a></p>';
+		_print ( contentHolder, imageHtml);
+	}
+	//alert ("End endGetAlbumPhotos");
+}
